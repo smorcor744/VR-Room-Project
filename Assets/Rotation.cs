e7659fbe4db8157516a6fcf6c2076b29af517ef2
smorcor744 <148891340+smorcor744@users.noreply.github.com>
2026-01-13 10:46:12 +0100

    1.3

--- Assets/Rotation.cs
+++ Assets/Rotation.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class Rotation : MonoBehaviour
 {
-    public float speed = 20f; // velocidad en grados por segundo
-    public Vector3 axis = new Vector3(0, 1, 0); // eje de rotación
+    public float speed = 100f; // velocidad en grados por segundo
+    public Vector3 axis = new Vector3(0, 0, 1); // eje de rotación
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(axis, speed * Time.deltaTime);
-
+        transform.Rotate(0f, speed * Time.deltaTime, 0f, Space.World);
     }
 }
+
